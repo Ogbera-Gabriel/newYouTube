7fdc4e458541155c8b7acf5e61773fbc3ae2805f
ogberagabriel234@gmail.com <ogberagabriel234@gmail.com>
2025-02-06 15:06:56 +0000

    Replace Geist font with Inter font in layout component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@/components/ui/button"
+import Image from 'next/image'
 
 
 export default function Home() {
   return (
     <div>
-      
+      <Image src="/logo.svg" height={50} width={50} alt="Logo"/>      
     </div>
   )
 }
